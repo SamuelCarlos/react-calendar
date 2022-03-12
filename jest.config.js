759128ba34785a119a116381e773334c480b10f3
Samuel Carlos <samuel_cxc@hotmail.com>
2022-03-12 09:09:47 -0300

    refactor: change App and Calendar pages to typescript

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,5 @@ module.exports = {
     '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
     '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': `<rootDir>/__mocks__/fileMock.js`
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleDirectories: ['node_modules', 'src']
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 }
---
+++ src/pages/Calendar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface CalendarProps {}
+
+export default function Calendar(props: CalendarProps) {
+  // your calendar implementation Goes here!
+  // Be creative
+  return (
+    <div className="container">
+      <h1>Calendar</h1>
+    </div>
+  )
+}
