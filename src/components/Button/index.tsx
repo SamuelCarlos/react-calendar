c65d686f6a67cc3ed4070be2f8d683ae37a0be8c
Samuel Carlos <samuel_cxc@hotmail.com>
2022-03-12 09:02:54 -0300

    refactor: change button to a folder and refator to typescript

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface ButtonProps {
+  onClick: () => void
+  children: React.ReactNode
+}
+
+export default function Button(props: ButtonProps) {
+  const { onClick, children } = props
+
+  return <button onClick={onClick}>{children}</button>
+}
