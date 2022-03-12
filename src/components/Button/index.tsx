575b1cabafa740e763a74875b7de4a6c397c3ed4
Samuel Carlos <samuel_cxc@hotmail.com>
2022-03-12 15:26:31 -0300

    feat: add material UI to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,22 @@
+import {
+  Button as MuiButton,
+  ButtonProps as MuiButtonProps
+} from '@mui/material'
+
 interface ButtonProps {
   onClick: () => void
   children: React.ReactNode
 }
 
 export default function Button(props: ButtonProps) {
-  const { onClick, children } = props
-
-  return <button onClick={onClick}>{children}</button>
+  const { onClick, children, ...other } = props
+  return (
+    <MuiButton
+      onClick={onClick}
+      variant="contained"
+      {...(other as MuiButtonProps)}
+    >
+      {children}
+    </MuiButton>
+  )
 }
