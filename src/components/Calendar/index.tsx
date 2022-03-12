d87fec491a99b373b76d8c3a8c662978169b522a
Samuel Carlos <samuel_cxc@hotmail.com>
2022-03-12 15:26:48 -0300

    feat: clean up Calendar component

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,29 +1,14 @@
-import Button from 'components/Button'
-import { daysInMonth, deserializeDate, firstDayOfMonth } from 'utils/dates'
-import { useAppDispatch, useAppSelector } from 'app/hooks'
-import { changeDay } from './calendar-slice'
+import Table from './Table'
+import { Typography } from '@mui/material'
 
 interface CalendarProps {
   selectedDay: string
 }
 export default function Calendar({ selectedDay }: CalendarProps) {
-  const dispatch = useAppDispatch()
-
-  const handleClick = (date: Date) => {
-    const ISODate = deserializeDate(date)
-    dispatch(changeDay(ISODate))
-  }
-
   return (
     <div data-testid="calendar">
-      Hi, this is my calendar, today is{' '}
-      {`${new Date(selectedDay).toLocaleDateString('en-US')}`}
-      <Button onClick={() => handleClick(new Date('11/11/1998'))}>
-        11/11/1998
-      </Button>
-      <Button
-        onClick={() => handleClick(new Date())}
-      >{`${new Date().toLocaleDateString('en-US')}`}</Button>
+      <Typography variant="h1">React Calendar</Typography>
+      <Table data={{ selectedDay: new Date(selectedDay) }} />
     </div>
   )
 }
