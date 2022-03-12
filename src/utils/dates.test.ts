76bcb1d697e78b90b60ac103cc2b384d03967b34
Samuel Carlos <samuel_cxc@hotmail.com>
2022-03-12 15:27:26 -0300

    feat: more dates utils

--- src/utils/dates.test.ts
+++ src/utils/dates.test.ts
@@ -1,4 +1,14 @@
-import { daysInMonth, firstDayOfMonth, deserializeDate } from './dates'
+import {
+  daysInMonth,
+  firstDayOfMonth,
+  deserializeDate,
+  getMonthName,
+  getYear,
+  lastDayOfMonth,
+  subtractDays,
+  addDays,
+  equalDates
+} from './dates'
 
 describe('Dates utils', () => {
   describe('daysInMonth()', () => {
@@ -10,10 +20,18 @@ describe('Dates utils', () => {
   })
 
   describe('firstDayOfMonth()', () => {
-    it('should return the week day of the first day of march/2022 month', () => {
+    it('should return the first day of march/2022 month', () => {
       const days = firstDayOfMonth(new Date('3/11/2022'))
 
-      expect(days).toBe('Tuesday')
+      expect(days.toLocaleDateString('en-US')).toBe('3/1/2022')
+    })
+  })
+
+  describe('lastDayOfMonth()', () => {
+    it('should return the last day of march/2022 month', () => {
+      const days = lastDayOfMonth(new Date('3/11/2022'))
+
+      expect(days.toLocaleDateString('en-US')).toBe('3/31/2022')
     })
   })
 
@@ -25,6 +43,49 @@ describe('Dates utils', () => {
       expect(ISOString).toMatch(
         /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/
       )
+    })
+  })
+
+  describe('getMonthName()', () => {
+    it('should return Mar for day 3/11/2022', () => {
+      const monthName = getMonthName(new Date('3/11/2022'))
+
+      expect(monthName).toBe('Mar')
+    })
+  })
+
+  describe('getYear()', () => {
+    it('should return 2022 for day 3/11/2022', () => {
+      const year = getYear(new Date('3/11/2022'))
+
+      expect(year).toBe(2022)
+    })
+  })
+
+  describe('subtractDays()', () => {
+    it('should return 1 day before day 3/11/2022', () => {
+      const date = subtractDays(new Date('3/11/2022'), 1)
+
+      expect(date.toLocaleDateString('en-US')).toBe('3/10/2022')
+    })
+  })
+
+  describe('addDays()', () => {
+    it('should return 1 day after day 3/11/2022', () => {
+      const date = addDays(new Date('3/11/2022'), 1)
+
+      expect(date.toLocaleDateString('en-US')).toBe('3/12/2022')
+    })
+  })
+
+  describe('equalDates()', () => {
+    it('should return true if sending two equal dates', () => {
+      const dateLeft = new Date('3/11/2022')
+      const dateRight = new Date('3/11/2022')
+
+      const isDatesEqual = equalDates(dateLeft, dateRight)
+
+      expect(isDatesEqual).toBe(true)
     })
   })
 })
--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,22 +1,71 @@
-import { getDaysInMonth, startOfMonth, format } from 'date-fns'
+import {
+  getDaysInMonth,
+  startOfMonth,
+  format,
+  getYear as dateFnsGetYear,
+  sub,
+  add,
+  endOfMonth,
+  isEqual
+} from 'date-fns'
 
-export type WeekDays =
-  | 'Sunday'
-  | 'Monday'
-  | 'Tuesday'
-  | 'Wednesday'
-  | 'Thursday'
-  | 'Friday'
-  | 'Saturday'
+export enum WeekDays {
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+}
+
+export type WeekDaysNames = keyof typeof WeekDays
+
+export const Months = {
+  1: 'Jan',
+  2: 'Feb',
+  3: 'Mar',
+  4: 'Apr',
+  5: 'May',
+  6: 'Jun',
+  7: 'Jul',
+  8: 'Aug',
+  9: 'Sep',
+  10: 'Oct',
+  11: 'Nov',
+  12: 'Dec'
+}
 
 export const daysInMonth = (date: Date) => {
   return getDaysInMonth(date)
 }
 
-export const firstDayOfMonth = (date: Date): WeekDays => {
-  return format(startOfMonth(date), 'EEEE') as WeekDays
+export const firstDayOfMonth = (date: Date) => {
+  return startOfMonth(date)
+}
+
+export const lastDayOfMonth = (date: Date) => {
+  return endOfMonth(date)
 }
 
 export const deserializeDate = (date: Date) => {
   return date.toISOString()
+}
+
+export const getMonthName = (date: Date) => {
+  return format(date, 'LLL')
+}
+
+export const getYear = (date: Date) => dateFnsGetYear(date)
+
+export const subtractDays = (date: Date, quantity: number) => {
+  return sub(date, { days: quantity })
+}
+
+export const addDays = (date: Date, quantity: number) => {
+  return add(date, { days: quantity })
+}
+
+export const equalDates = (dateLeft: Date, dateRight: Date) => {
+  return isEqual(dateLeft, dateRight)
 }
